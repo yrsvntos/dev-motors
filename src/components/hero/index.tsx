932e66ed1dc59dd763b2114ba54a3fc3191d6285
Yanik dos Santos <yrsvntos@gmail.com>
2025-10-16 18:47:04 +0100

    feat: adicionar o componente Hero

---
+++ src/components/hero/index.tsx
@@ -0,0 +1,45 @@
+
+import styles from './styles.module.scss'
+import Image from 'next/image';
+import { ReactNode } from 'react';
+
+interface HeroProps{
+    heading: string;
+    buttonUrl: string;
+    buttonTitle: string;
+    bannerUrl: string;
+    icon: ReactNode
+
+}
+
+export function Hero({heading, bannerUrl, buttonTitle, buttonUrl, icon}: HeroProps){
+    return(
+        <main className={styles.main}>
+            <div className={styles.containerHero}>
+                <h1 className={styles.title}>{heading}
+                    
+                </h1>
+                <a 
+                    href={buttonUrl}
+                    target='_blank'
+                    className={styles.link}
+                >
+                    {icon}
+                    {buttonTitle}
+                </a>
+            </div>
+
+            <div className={styles.contentBanner}>
+                <Image
+                
+                    alt={heading}
+                    src={bannerUrl}
+                    priority={true}
+                    quality={100}
+                    fill={true}
+                    className={styles.banner}
+                />
+            </div>
+        </main>
+    )
+}
