8a75b0fa95e87d647cacf4f94e1a16e1251b8046
Yanik dos Santos <yrsvntos@gmail.com>
2025-10-20 17:42:28 +0100

    feat(page): adicionar o componente submenu na página principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,31 @@
 import { Hero } from "@/components/hero";
 import { Services } from "@/components/home/services";
 import { SubMenu } from "@/components/home/submenu";
-import { getDataHome } from "@/utils/actions/get-data";
+import { getDataHome, getSubmenu } from "@/utils/actions/get-data";
 import { HomeProps } from "@/utils/home-type";
 import { Phone } from "lucide-react";
 import { Container } from "@/components/container";
 import { Footer } from "@/components/home/footer";
+import { MenuProps } from "@/utils/menu.type";
 
 
 export default async function Home() {
 
   const {object}: HomeProps = await getDataHome();
+  const menu: MenuProps = await getSubmenu();
+ 
 
-  console.log(object.title)
+  // console.log(object.title)
   return (
     <main>
-      <SubMenu/>
+      {menu.objects.length > 0 && <SubMenu menu={menu}/>}
       <Hero
         heading={object.metadata.heading}
         buttonTitle={object.metadata.cta_button.title}
         buttonUrl={object.metadata.cta_button.url}
         bannerUrl={object.metadata.banner.url}
         icon={<Phone size={24} color='#fff'/>}
+        
       />
       <Container>
         <Services
