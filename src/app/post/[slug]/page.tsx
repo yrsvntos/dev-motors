9dd8d976d5ab18457dd44a55aff8b591aedb510a
Yanik dos Santos <yrsvntos@gmail.com>
2025-10-20 17:33:51 +0100

    feat: mostrar informações sobre as páginas dinâmicas

---
+++ src/app/post/[slug]/page.tsx
@@ -0,0 +1,65 @@
+import { ReactNode } from 'react'
+import styles from './styles.module.scss'
+import { getItemBySlug } from '@/utils/actions/get-data'
+import { PostProps } from '@/utils/post.type'
+import { Hero } from '@/components/hero'
+import { Phone } from 'lucide-react'
+import { Container } from '@/components/container'
+import Image from 'next/image'
+
+
+export default async function Page({
+    params,
+  }: {
+    params: Promise<{ slug: string }>
+  }){
+    const { slug } = await params
+    const {objects}: PostProps = await getItemBySlug(slug)
+    // console.log(JSON.stringify(objects, null, 2))
+
+    
+    return(
+        <>
+            <Hero
+              heading={objects[0].title}
+              buttonTitle={objects[0].metadata.button.title}
+              buttonUrl={objects[0].metadata.button.url}
+              bannerUrl={objects[0].metadata.banner.url}
+              icon={<Phone size={24} color='#fff'/>}
+            />
+
+            <Container>
+              <section className={styles.about}>
+                <article className={styles.innerAbout}>
+                  <h1 className={styles.title}>
+                    {objects[0].metadata.description.title}
+                  </h1>
+                  <p>
+                    {objects[0].metadata.description.text}
+                  </p>
+                    {objects[0].metadata.description.button_active && (
+                      <a 
+                        href={objects[0].metadata.button.url as string}
+                        target='_blank'
+                        className={styles.link}
+                      >
+                        {objects[0].metadata.button.title}
+                      </a>
+                    )}
+                </article>
+                <div className={styles.bannerAbout}>
+                  <Image
+                      className={styles.imageAbout}
+                      alt={objects[0].title}
+                      quality={100}
+                      fill={true}
+                      priority={true}
+                      sizes='(max-width: 480px) 100vw, (max-width: 1024px) 75vw, 50vw'
+                      src={objects[0].metadata.description.banner.url}
+                  />
+                </div>
+              </section>
+            </Container>
+        </>
+    )
+}
